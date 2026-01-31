aa8a140b0609f1a349a15052bf36fbc92dd9d50f
Ewan <ewan.connan@phs.org.za>
2026-01-31 14:17:14 +0200

    Delphi recovery save

---
+++ Delphi/__recovery/SIP_GUI.pas
@@ -0,0 +1,162 @@
+unit SIP_GUI;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.Imaging.jpeg, Vcl.StdCtrls, Vcl.ExtCtrls, ShellApi, FileCtrl;
+
+type
+  TForm1 = class(TForm)
+    btnTop: TButton;
+    btnFront: TButton;
+    btnRight: TButton;
+    pnlPreview: TPanel;
+    imgPreview3DModel: TImage;
+    lblPreview: TLabel;
+    btnGenerate: TButton;
+    pnlFrontView: TPanel;
+    imgFront: TImage;
+    pnlTopView: TPanel;
+    pnlRightView: TPanel;
+    imgTop: TImage;
+    imgSide: TImage;
+    lblTitle: TLabel;
+    edtName: TEdit;
+    lblName: TLabel;
+    bntSelectPath: TButton;
+    tmr10ms: TTimer;
+    procedure btnFrontClick(Sender: TObject);
+    procedure btnTopClick(Sender: TObject);
+    procedure btnRightClick(Sender: TObject);
+    procedure btnGenerateClick(Sender: TObject);
+    procedure bntSelectPathClick(Sender: TObject);
+    procedure tmr10msTimer(Sender: TObject);
+  private
+    var
+      sImgPathTopView, sImgPathFrontView, sImgPathRightView, sSavePath : String;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.bntSelectPathClick(Sender: TObject);
+var
+  ChosenDirectory: string;
+begin
+  // Show dialog starting from C:\, with custom title
+  if SelectDirectory('Select a Folder', 'C:\', ChosenDirectory) then
+  begin
+    ShowMessage('Selected: ' + ChosenDirectory);
+    sSavePath := ChosenDirectory;
+  end
+  else
+    ShowMessage('Selection cancelled');
+
+end;
+
+procedure TForm1.btnFrontClick(Sender: TObject);
+var
+  OpenDialog: TOpenDialog;
+begin
+  OpenDialog := TOpenDialog.Create(nil);
+  try
+
+    OpenDialog.Filter := 'Image files|*.png;*.jpg;*.jpeg;*.bmp';
+    OpenDialog.Options := [ofFileMustExist];
+
+    if OpenDialog.Execute then
+    begin
+      imgFront.Picture.LoadFromFile(OpenDialog.FileName);
+      imgFront.Center := True;
+      imgFront.Stretch := True;
+      sImgPathFrontView := OpenDialog.FileName;
+    end;
+  finally
+    OpenDialog.Free;
+  end;
+
+end;
+
+procedure TForm1.btnGenerateClick(Sender: TObject);
+var
+  sPythonCommand: String;
+begin
+  sPythonCommand := 'C:\Not_Onedrive\GitHub\SIP-Project-2026\Python\V3.py 0';
+  sPythonCommand := sPythonCommand + ' ' + sImgPathTopView;
+  sPythonCommand := sPythonCommand + ' ' + sImgPathFrontView;
+  sPythonCommand := sPythonCommand + ' ' + sImgPathRightView;
+  sPythonCommand := sPythonCommand + ' ' + edtName.Text;
+  sPythonCommand := sPythonCommand + ' ' + sSavePath;
+  // Run an executable with parameters
+  ShellExecute(0, 'open', 'python.exe', PWideChar(sPythonCommand), nil, SW_HIDE);
+
+end;
+
+procedure TForm1.btnRightClick(Sender: TObject);
+var
+  OpenDialog: TOpenDialog;
+begin
+  OpenDialog := TOpenDialog.Create(nil);
+  try
+
+    OpenDialog.Filter := 'Image files|*.png;*.jpg;*.jpeg;*.bmp';
+    OpenDialog.Options := [ofFileMustExist];
+
+    if OpenDialog.Execute then
+    begin
+      imgSide.Picture.LoadFromFile(OpenDialog.FileName);
+      imgSide.Center := True;
+      imgSide.Stretch := True;
+      sImgPathRightView := OpenDialog.FileName;
+    end;
+  finally
+    OpenDialog.Free;
+  end;
+
+end;
+
+procedure TForm1.btnTopClick(Sender: TObject);
+var
+  OpenDialog: TOpenDialog;
+begin
+  OpenDialog := TOpenDialog.Create(nil);
+  try
+
+    OpenDialog.Filter := 'Image files|*.png;*.jpg;*.jpeg;*.bmp';
+    OpenDialog.Options := [ofFileMustExist];
+
+    if OpenDialog.Execute then
+    begin
+      imgTop.Picture.LoadFromFile(OpenDialog.FileName);
+      imgTop.Center := True;
+      imgTop.Stretch := True;
+      sImgPathTopView := OpenDialog.FileName;
+    end;
+  finally
+    OpenDialog.Free;
+  end;
+
+end;
+
+procedure TForm1.tmr10msTimer(Sender: TObject);
+begin
+  if (sImgPathRightView <> '') and (sImgPathTopView <> '') and (sImgPathFrontView <> '') and (sSavePath <> '') and (edtName.Text <> '') then
+  begin
+    btnGenerate.Enabled := True;
+  end
+  else
+  begin
+    btnGenerate.Enabled := False;
+  end;
+
+end;
+
+end.
